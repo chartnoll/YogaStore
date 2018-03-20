ca423f4bb9bbd57d62891ce78b44df91bc342012
Chris Hartnoll <cvhjersey@gmail.com>
2018-03-20 16:28:17 +0100

    First success!

---
+++ yogastore.js
@@ -0,0 +1,18 @@
+
+var products = [
+  { referenceNumber: 1231, name: "Super Lite Mat", price: 10 },
+  { referenceNumber: 1232, name: "Power Mat", price: 20 },
+  { referenceNumber: 1233, name: "Block", price: 30 },
+  { referenceNumber: 1234, name: "Meditation cushion", price: 30 },
+  { referenceNumber: 1235, name: "The best T-shirt", price: 200 },
+  { referenceNumber: 1236, name: "The cutest yoga pants", price: 300 },
+  { referenceNumber: 1237, name: "Bring Yoga To Life", price: 30 },
+  { referenceNumber: 1238, name: "Light On Yoga", price: 10 }
+]
+
+for ( var index = 0 ; index <= products.length ; index ++ ) {
+  document.write(`${products[index].name} reference number
+    ${products[index].referenceNumber} <br>`);
+}
+
+document.write('Please enter the product reference number: <br>');
